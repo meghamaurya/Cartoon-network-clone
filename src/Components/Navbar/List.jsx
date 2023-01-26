476fee13b52c42c31ac728b26156634ba06d7a6d
megha <meghamini40@gmail.com>
2023-01-26 15:35:09 +0530

    component for nav list

---
+++ src/Components/Navbar/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const List = () => {
+  return (
+    <ul className="list">
+      <li>GAMES</li>
+      <li>VIDEOS</li>
+      <li>APPS</li>
+      <li>TOON CUP</li>
+      <li>CLIMATE CHAMPIONS</li>
+    </ul>
+  );
+};
