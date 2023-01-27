300e0d60c9de3e605b6f7b105263cef23dd51bf6
megha <meghamini40@gmail.com>
2023-01-27 17:06:16 +0530

    Icon list

--- src/Components/Navbar/List.jsx
+++ src/Components/Navbar/List.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 export const List = () => {
+  const listItem = ["Games", "VIDEOS", "APPS", "TOON CUP", "CLIMATE CHAMPIONS"];
   return (
     <ul className="list">
-      <li>GAMES</li>
-      <li>VIDEOS</li>
-      <li>APPS</li>
-      <li>TOON CUP</li>
-      <li>CLIMATE CHAMPIONS</li>
+      {listItem.map((item, i) => {
+        return (
+          <div key={i}>
+            <li className="list">{item}</li>
+          </div>
+        );
+      })}
     </ul>
   );
 };
