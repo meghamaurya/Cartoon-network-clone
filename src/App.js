fd03d5a940e164ea9aab5f5a7f752fc236159931
megha <meghamini40@gmail.com>
2023-01-28 01:48:50 +0530

    render second nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import SecondNav from './Components/SecondNav/SecondNav';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SecondNav />
     </div>
   );
 }
