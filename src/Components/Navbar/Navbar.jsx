3f758506fa0ed53a8ce3d47c6d7008c950309468
megha <meghamini40@gmail.com>
2023-01-28 18:09:26 +0530

    fixed some css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
       <img
         src="https://cncdn.dmti.cloud/200e4a4b5c610e7faf2ade024293655cdbae5a40/static/media/cn-logo.3f4d7cfb.svg"
         alt="logo"
-        width="100rem"
-        style={{ display: "block" }}
+        // width="120rem"
+        className="logo"
       />
       {!isNavExpanded ? (
         <IoMdMenu onClick={handleClick} className="hamburger-icon" />
