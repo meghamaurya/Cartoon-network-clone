235491dc2eff836234839a5e22a1fb1d0c20e3df
megha <meghamini40@gmail.com>
2023-02-03 12:21:42 +0530

    btn comp

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ImArrowRight2 } from "react-icons/im";
+
+import "./Styles.css";
+
+const Button = ({ Icon, title, bgColor, width }) => {
+  //   console.log(props.bgClr, "btn");
+  return (
+    <div
+      className="btnContainer"
+      style={{ backgroundColor: `${bgColor}`, width: `${width}` }}
+    >
+      {Icon ? <Icon className="gameremote" /> : " "}
+      <h1 className="gameTitle">{title}</h1>
+      <ImArrowRight2 className="arrow" />
+    </div>
+  );
+};
+
+export default Button;
