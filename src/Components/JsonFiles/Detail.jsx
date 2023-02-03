aab99eb8bbd222ce8a728d7abd49df7315bef5ff
megha <meghamini40@gmail.com>
2023-02-04 00:36:19 +0530

    seperate folder for all json files

---
+++ src/Components/JsonFiles/Detail.jsx
@@ -0,0 +1,34 @@
+var Detail = {
+  img1: "https://cn.i.cdn.ti-platform.com/content/2187/submit-your-creation/link/za/ryw_bannercms_gallery.77ae4f1b.jpg?imwidth=420",
+  title1: "REDRAW YOUR WORLD GALLERY",
+  para1:
+    "You showed us how you Redraw Your World in so many amazing different ways. Check out the gallery below to get inspired by great entries from around Africa!",
+  btntitle1: "CHECK ALL GAMES",
+  headTitle1: "Featured games",
+  img2: "https://cn.i.cdn.ti-platform.com/content/92/climate-champions---school/link/za/climatechamps-1600x900-en.44c6ccbc.jpg?imwidth=640",
+  title2: "CLIMATE CHAMPIONS - SCHOOL",
+  para2: ".",
+  btntitle2: "CHECK ALL VIDEOS",
+  headTitle2: "TOP VIDEOS",
+  img3: "https://cn.i.cdn.ti-platform.com/content/20/cartoon-network-gamebox/link/za/gamebox-cms-1600x900.2307313b.png?imwidth=640",
+  title3: "CARTOON NETWORK GAMEBOX",
+  para3:
+    "ALL YOUR FAVOURITE SHOWS ARE HERE! Play games with the BEST Cartoon Network shows.",
+  btntitle3: "EXPLORE EVERYTHING TEEN TITANS GO!",
+  headTitle3: "Teen Titans Go!",
+  bgBmx:
+    "https://cn.i.cdn.ti-platform.com/content/437/showpage/teen-titans-go%21/za/ttg-backgroundtile-min.png?imwidth=",
+  bgCraig:
+    "https://cn.i.cdn.ti-platform.com/cnemea/content/1540/showpage/craig-of-the-creek/uk/backgroundtile.png?imwidth=",
+  btntitle4: "EXPLORE EVERYTHING CRAIG OF THE CREEK",
+  headTitle4: "Craig of the Creek",
+  CNPara:
+    "Cartoon Network is home to your favourite cartoons and free games. Play games online with Cartoon Network characters from Ben 10, Adventure Time, Apple and Onion, Gumball, The Powerpuff Girls and more. You can also watch the best videos of your favourite shows including We Bare Bears, Teen Titans Go and Ivandoe.",
+  CNlogo:
+    "https://cncdn.dmti.cloud/200e4a4b5c610e7faf2ade024293655cdbae5a40/static/media/cn-logo-short.1e292202.svg",
+  CNDiscovery:
+    "https://cncdn.dmti.cloud/200e4a4b5c610e7faf2ade024293655cdbae5a40/static/media/wbd-logo-white.5ff10580.svg",
+  CNFullForm:
+    "https://cncdn.dmti.cloud/200e4a4b5c610e7faf2ade024293655cdbae5a40/static/media/cn-logo.7f0ade13.svg",
+};
+export default Detail;
